f709fa195d55dbf5ff349d675b2c1643d4cbe7c9
Tcheny <eugenie.be@hotmail.fr>
2017-10-28 15:49:31 +0200

    schema comment

--- back/src/models/CommentModel.js
+++ back/src/models/CommentModel.js
@@ -3,9 +3,21 @@ import mongoose from 'mongoose'; // middleware mongoose
 // Création du Schema et model
 const Schema = mongoose.Schema;
 let NewComment = new Schema({
+  author : {
+    type : String,
+    required : false
+  },
   title : {
+  type : String,
+  required : true
+  },
+  comment : {
     type :  String,
     required : true
+  },
+  date : {
+    type : Date,
+    default: Date.now
   },
   blog : [
     {
