42ca3ec0afdf5d4841eba5c3f3fc7ef7e8c6dd24
Tcheny <eugenie.be@hotmail.fr>
2017-10-28 16:17:28 +0200

    delete comment

---
+++ back/src/controller/deleteComment.js
@@ -0,0 +1,22 @@
+import express from 'express';
+
+import Article from '../models/ArticleModel';
+import Comment from '../models/CommentModel';
+
+const api = express.Router();
+
+// GET
+// Supprimer un commentaire d'un article
+// route /app/blog/:id/comment/:commentid/delete
+// ============================
+api.get('/:id/comment/:commentid/delete', ( req, res ) => {
+  Comment.findByIdAndRemove(req.params.commentid, ( err, deleteComment ) => {
+    if ( err ) return res.send( err );
+    Article.findByIdAndUpdate( req.params.id, { $pull : { comment : deleteComment._id }}, err => {
+      if ( err ) return res.send( err );
+      res.json({ message : `Comment has been deleted` });
+    });
+  });
+});
+
+export default api;
--- back/src/controller/main.js
+++ back/src/controller/main.js
@@ -8,6 +8,7 @@ import UpdateArticle from './updateArticle';
 import DeleteArticle from './deleteArticle';
 import PostComment from './postComment';
 import GetComment from './getComment';
+import DeleteComment from './deleteComment';
 
 const api = express.Router();
 
@@ -18,5 +19,6 @@ api.use( UpdateArticle );
 api.use( DeleteArticle );
 api.use( PostComment );
 api.use( GetComment );
+api.use( DeleteComment );
 
 export default api;
