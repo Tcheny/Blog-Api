8a295a1519ab1661e6a85b8d86c81bc27c2d67f4
Tcheny <eugenie.be@hotmail.fr>
2017-10-28 11:58:25 +0200

    remove body-parser

--- back/src/index.js
+++ back/src/index.js
@@ -1,5 +1,4 @@
 import express from 'express'; // middleware express
-import bodyParser from 'body-parser'; // middleware bodyParser pour parser urlencoded
 import cors from 'cors'; // middleware cors
 
 import config from './config/config'; // config pour le port 8080
@@ -8,7 +7,7 @@ import route from './route/route'; // route
 const app = express();
 
 // Parse le formulaire 'urlencoded'
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
 // Utilise cors
 app.use(cors());
 // Utilise la route '/app...' puis appelle les autres routes du controller
