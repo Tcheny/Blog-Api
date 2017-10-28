6d0dbe2eec97556f44cfea3c861bcb55e755921e
Tcheny <eugenie.be@hotmail.fr>
2017-10-28 15:50:41 +0200

    get comment

---
+++ back/src/controller/getComment.js
@@ -0,0 +1,21 @@
+import express from 'express';
+
+import Article from '../models/ArticleModel';
+import Comment from '../models/CommentModel';
+
+const api = express.Router();
+
+// GET
+// Récupérer les commentaires d'un article
+// route /app/blog/:id/comment/
+// ============================
+api.get('/:id/comment', ( req, res ) => {
+  Article.findById( req.params.id )
+         .populate('comment')
+         .exec(( err, article ) => {
+           if ( err ) return res.send( err );
+           res.send( article.comment );
+         } );
+});
+
+export default api;
